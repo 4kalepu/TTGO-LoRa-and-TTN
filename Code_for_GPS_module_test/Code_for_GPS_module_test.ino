74e5191390b98f6f087d39b32f28ec108c428018
kalepu <52924039+kalepu4@users.noreply.github.com>
2021-07-10 18:39:44 +0530

    sdvfsd

---
+++ Code_for_GPS_module_test/Code_for_GPS_module_test.ino
@@ -0,0 +1,52 @@
+#include <TinyGPS++.h>
+TinyGPSPlus gps;
+HardwareSerial SerialX(1);
+void setup(){
+
+Serial.begin(115200); 
+SerialX.begin(9600, SERIAL_8N1, 12, 15);
+}
+void loop()
+{
+
+Serial.print("Latitude   :   ");
+Serial.println (gps.location.lat(), 5); 
+
+Serial.print ("Longitude : ");
+Serial.println (gps.location. lng(), 4);
+
+Serial.print("Satellites: ");
+Serial.println (gps.satellites.value());
+
+Serial.print("Altitude : ");
+Serial.print (gps.altitude.feet() / 3.2808);
+
+Serial.println("M");
+Serial.print("Time      : ");
+
+Serial.print (gps.time.hour());
+
+Serial.print(":");
+
+Serial.print (gps.time.minute());
+
+Serial.print(":"); 
+Serial.println (gps.time. second());
+
+Serial.println("*****************");
+
+smartDelay(1000);
+
+if (millis() > 5000 && gps.charsProcessed() <10)
+Serial.println (F("No GPS data received: check wiring"));
+}
+static void smartDelay(unsigned long ms)
+{ 
+unsigned long start = millis();
+do
+{
+while (SerialX.available())
+gps.encode (SerialX.read()); 
+} while (millis()- start < ms);
+
+}
